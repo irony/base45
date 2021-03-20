9f9c683ce6d413a4a7e63ced5a4bb6d58eec0fa8
Christian Landgren <christian@landgren.nu>
2021-03-20 18:05:18 +0100

    chore: enforce eslint

--- __tests__/encode.js
+++ __tests__/encode.js
@@ -1,4 +1,5 @@
-const encode = require('../lib/encode')
+/* eslint-disable no-undef */
+const { encode } = require('../lib/base45')
 const cases = [
   ['Hello!!', '%69 VD92EX0'],
   ['AB', 'BB8'],
@@ -6,13 +7,13 @@ const cases = [
   ['ietf!', 'QED8WEX0'],
   [
     'The quick brown fox jumps over the lazy dog',
-    '8UADZCKFEOEDJOD2KC54EM-DX.CH8FSKDQ$D.OE44E5$CS44+8DK44OEC3EFGVCD2',
+    '8UADZCKFEOEDJOD2KC54EM-DX.CH8FSKDQ$D.OE44E5$CS44+8DK44OEC3EFGVCD2'
   ]
 ]
 
 describe('encoding', () => {
   test('all test cases encode correctly', () => {
-    cases.map(([text, expected]) => {
+    cases.forEach(([text, expected]) => {
       const result = encode(text)
       expect(result).toEqual(expected)
     })
